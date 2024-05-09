13bba437429ff94bd88500c3dcc13a9379bd3eb7
Georgiana Lungu <l.georgiana2901@gmail.com>
2024-05-10 00:28:29 +0300

    Modify form route

--- app/api/form/route.ts
+++ app/api/form/route.ts
@@ -1,26 +1,3 @@
-// import sgMail from '@sendgrid/mail';
-// import { NextResponse, NextRequest } from "next/server";
-
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-
-// export async function POST(req: NextRequest) {
-//     const { name, email, message }: { name: string; email: string; message: string } = await req.json();
-//     const msg = {
-//         to: 'lungugeorgiana20@stud.ase.ro',
-//         from: 'cojocaruandreea186@gmail.com',
-//         subject: `${name.toUpperCase()} send you a email`,
-//         text: `Email => ${email}`,
-//         html: `<strong> ${message}</sytong>`,
-//     };
-
-//     try {
-//         await sgMail.send(msg);
-//         return NextResponse.json({ success: true }, { status: 200 });
-//     } catch (error) {
-//         return NextResponse.json({ success: false }, { status: 500 });
-//     }
-// }
-
 import sgMail from '@sendgrid/mail';
 import { NextResponse, NextRequest } from "next/server";
 
@@ -36,12 +13,11 @@ export async function POST(req: NextRequest) {
         html: `<strong> ${message}</sytong>`,
     };
 
-    sgMail.send(msg)
-        .then(() => {
-            return NextResponse.json({ success: true }, { status: 200 });
-        })
-        .catch((error) => {
-            console.error(error);
-            return NextResponse.json({ success: false }, { status: 500 });
-        });
+    try {
+        await sgMail.send(msg);
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ success: false }, { status: 500 });
+    }
 }
+
